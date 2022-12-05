3e49731386584b520315794c69fe2b471e06b929
Mitch Koko <mitchkoko@Mitchs-MacBook-Pro.local>
2022-12-05 18:42:03 +1100

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of tiles
+  final List myTiles = [
+    'A',
+    'B',
+    'C',
+    'D',
+  ];
+
+  // reorder method
+  void updateMyTiles(int oldIndex, int newIndex) {
+    setState(() {
+      // this adjustment is needed when moving down the list
+      if (oldIndex < newIndex) {
+        newIndex -= 1;
+      }
+
+      // get the tile we are moving
+      final String tile = myTiles.removeAt(oldIndex);
+      // place the tile in new position
+      myTiles.insert(newIndex, tile);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Re-Orderable ListView")),
+      body: ReorderableListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          for (final tile in myTiles)
+            Padding(
+              key: ValueKey(tile),
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.grey[200],
+                child: ListTile(
+                  title: Text(tile.toString()),
+                ),
+              ),
+            ),
+        ],
+        onReorder: (oldIndex, newIndex) {
+          updateMyTiles(oldIndex, newIndex);
+        },
+      ),
+    );
+  }
+}
